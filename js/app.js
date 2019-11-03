e39574d1416df45ec256faa4f1175ab5993d5a89
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2019-11-03 21:43:58 +0100

    Add alert message

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+// const closeButton = document.getElementById('close');
+
+// // Hide alert message
+// closeButton.addEventListener('click', (e) => {
+//     const close = e.target;
+//     const alertMessage = close.parentNode.parentNode;
+//     alertMessage.style.display = 'none';
+// });
+
+// Hide alert message
+$('#close').click(function () {
+    $('.alert-message').slideUp();
+});
