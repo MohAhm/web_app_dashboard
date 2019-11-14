954c3a508308879fd636d0b341afea1a78a6e6d7
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2019-11-14 20:14:05 +0100

    Display a different line chart on click.

--- js/chart.js
+++ js/chart.js
@@ -1,3 +1,5 @@
+const chartNav = document.querySelector('.chart-nav');
+
 // set global variables of the Chart
 Chart.defaults.global.defaultFontFamily = 'Open Sans';
 Chart.defaults.global.defaultFontColor = '#8e8e8e';
@@ -8,15 +10,31 @@ Chart.defaults.global.tooltips.enabled = false;
 Chart.defaults.global.maintainAspectRatio = false;
 Chart.defaults.global.aspectRatio = 1;
 
+const trafficLabels = [
+    ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+    ['Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+    ['Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
+];
+
+const trafficData = [
+    [25, 30, 15, 35, 20, 45, 20, 15, 35, 15, 20],
+    [220, 350, 300, 156, 250, 460, 300, 260, 400, 350, 200],
+    [500, 1000, 750, 1250, 1750, 1250, 1000, 1500, 2000, 1500, 2000],
+    [2500, 1750, 2300, 3000, 4700, 4400, 3500, 2800, 1000, 1500, 2200]
+];
+
+const scales = [50, 500, 2500, 5000];
+
 // create traffic chart ---
 let lineChart = new Chart($('#traffic'), {
     type: 'line',
 
     data: {
-        labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+        labels: trafficLabels[2],
 
         datasets: [{
-            data: [500, 1000, 750, 1250, 1750, 1250, 1000, 1500, 2000, 1500, 2000],
+            data: trafficData[2],
 
             backgroundColor: 'rgba(116, 119, 191, 0.25)',
             borderColor: '#7377BF',
@@ -37,12 +55,50 @@ let lineChart = new Chart($('#traffic'), {
             yAxes: [{
                 ticks: {
                     max: 2500,
+                    stepSize: 500,
                     beginAtZero: true
                 }
             }]
         }
     }
 });
+
+function updateData(chart, labels, data) {
+    chart.data.labels = labels;
+
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data = data;
+    });
+
+    chart.update();
+}
+
+function updateScale(chart, maxNum) {
+    chart.options.scales.yAxes[0] = {
+        ticks: {
+            max: maxNum,
+            stepSize: maxNum / 5,
+            beginAtZero: true
+        }
+    };
+}
+
+chartNav.addEventListener('click', (e) => {
+    const list = chartNav.querySelectorAll('li');
+    list.forEach(li => {
+        li.className = 'chart-nav__link';
+    });
+
+    if (e.target.tagName === 'A') {
+        const link = e.target.parentNode;
+        const i = $(link).index();
+
+        updateScale(lineChart, scales[i]);
+        updateData(lineChart, trafficLabels[i], trafficData[i]);
+        link.className += '--current';
+    }
+});
+
 
 
 // create daily traffic chart ---
